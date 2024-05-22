031e9b31f37d9180d7c3467ae0310d36195b4f5a
Areeb-Ali <areebithink@gmail.com>
2024-05-22 18:32:54 +0500

    Scripting Completed

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
 const email = document.querySelector("#email");
-const userName = document.querySelector("#username");
+const username = document.querySelector("#username");
 const cnic = document.querySelector("#cnic");
 const password = document.querySelector("#password");
+const age = document.querySelector("#age");
+const form = document.querySelector("#form");
 
+let usernameRegex = /^[a-zA-Z0-9]{4,15}$/;
+let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+let passwordRegex = /^(?=.*[A-Z])(?=.*\W)[a-zA-Z\d\W]{8,}$/;
+let cnicPattern = /^[0-4]{1}[0-9]{2}(-\d{7}-\d{3})?$/;
+let ageRegex = /^(1[8-9]|[2-5][0-9]|60)$/;
+let notSame = "It does not meet the validation.";
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (usernameRegex.test(username.value)) {
+    console.log(username.value);
+  }
+  //else {
+  //console.log(notSame);
+  //}
+
+  if (emailRegex.test(email.value)) {
+    console.log(email.value);
+  } else {
+    console.log(notSame);
+  }
+
+  if (passwordRegex.test(password.value)) {
+    console.log(password.value);
+  } else {
+    console.log(notSame);
+  }
+
+  if (cnicPattern.test(cnic.value)) {
+    console.log(cnic.value);
+  } else {
+    console.log(notSame);
+  }
+
+  if (ageRegex.test(age.value)) {
+    console.log(age.value);
+  } else {
+    console.log(notSame);
+  }
+});
