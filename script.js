1121c48f5bb0325bd95e502585439f8394e64151
Areeb-Ali <areebithink@gmail.com>
2024-05-22 17:11:38 +0500

    Basic Structure Completed

---
+++ script.js
@@ -0,0 +1,5 @@
+const email = document.querySelector("#email");
+const userName = document.querySelector("#username");
+const cnic = document.querySelector("#cnic");
+const password = document.querySelector("#password");
+
